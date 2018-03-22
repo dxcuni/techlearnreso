16db7d249cbbe6d9c00d74b2820d45dbbe30556e
pibanez81 <36833110+pibanez81@users.noreply.github.com>
2018-03-22 15:46:10 -0600

    admin11

---
+++ res/json/TechnicalAdmin02.js
@@ -0,0 +1,1 @@
+conf = '[{"name":"Page2Admin02","description":"Page2Admin02","mode":"1","url":"http://"},{"name":"Admin02Mode3","description":"Admin02Mode3","mode":"3","url":"https://my.dxc.com/"}]';
